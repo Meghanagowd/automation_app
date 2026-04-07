8b2a3eac5384c2c72b3fb5328173b7be29149dca
Meghana_S <144104378+Meghanagowd@users.noreply.github.com>
2026-04-07 06:09:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello! Docker is running successfully.,trigger"
+    return "Hello! Docker is running successfully."
 
 if __name__ == "__main__":
     # host="0.0.0.0" is essential for Docker to map ports correctly
